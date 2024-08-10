1b9e841ab1c66276817103751ac1a9daa31b33ab
fabasoad <fabasoad@gmail.com>
2024-08-10 23:35:32 +0900

    Add macOS support 4

--- src/install-swipl-windows.sh
+++ src/install-swipl-windows.sh
@@ -8,8 +8,8 @@ expected_sha="958ac9f0ac2622abd69ba0df8615e23ef9e3c1e2dd7b4589841c2db0ff5089f1" 
 curl -O "https://www.swi-prolog.org/download/stable/bin/${swipl_exe_name}"
 actual_sha="$(certutil -hashfile "${swipl_exe_name}" SHA256)"
 
-if [ "${expected_sha}" != "${actual_sha}" ]; then
-  msg="Expected SHA256: ${expected_sha}. Actual SHA256: ${actual_sha}"
+if ! certutil -hashfile "${swipl_exe_name}" SHA256 | grep "${expected_sha}"; then
+  msg="Expected SHA256 and actual SHA256 signatures are different"
   echo "::error title=Binary is corrupted::${msg}" && exit 1
 fi
 
