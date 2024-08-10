3bb096640624500077d787336d8102261913ef4c
fabasoad <fabasoad@gmail.com>
2024-08-10 23:30:43 +0900

    Add macOS support 2

--- src/install-swipl-windows.sh
+++ src/install-swipl-windows.sh
@@ -6,7 +6,7 @@ swipl_exe_name="swipl-${SWIPL_VERSION}-1.x64.exe"
 expected_sha="958ac9f0ac2622abd69ba0df8615e23ef9e3c1e2dd7b4589841c2db0ff5089f1" # pragma: allowlist secret
 
 curl -O "https://www.swi-prolog.org/download/stable/bin/${swipl_exe_name}"
-actual_sha="$(shasum -a 256 "${swipl_exe_name}")"
+actual_sha="$(certutil -hashfile "${swipl_exe_name}" SHA256)"
 
 if [ "${expected_sha}" != "${actual_sha}" ]; then
   msg="Expected SHA256 signature and actual one are different"
