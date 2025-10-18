518847d9563806a08450530fe537f67ae750a8d0
fabasoad <fabasoad@gmail.com>
2025-10-18 15:44:03 +0900

    fix: opt 2

--- src/install-swipl/windows.sh
+++ src/install-swipl/windows.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-winget install --id SWI-Prolog.SWI-Prolog -e --accept-package-agreements --accept-source-agreements
+#winget install --id SWI-Prolog.SWI-Prolog -e --accept-package-agreements --accept-source-agreements
+choco install swi-prolog
