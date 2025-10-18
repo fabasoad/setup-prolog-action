b11d84983053342a40e495957a9eda4943f4a041
fabasoad <fabasoad@gmail.com>
2025-10-18 15:42:04 +0900

    fix: opt 1

--- src/install-swipl/windows.sh
+++ src/install-swipl/windows.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env sh
 
-main() {
-  bin_dir="${RUNNER_TEMP}/bin"
-  mkdir -p "${bin_dir}"
-  if [ "${RUNNER_ARCH}" = "X86" ]; then
-    arch="x86"
-  else
-    arch="x64"
-  fi
-  url="https://www.swi-prolog.org/download/stable/bin/swipl-9.2.9-1.${arch}.exe"
-  curl -sLk "${url}" -o "${bin_dir}/swipl.exe"
-  echo "${bin_dir}" >> "$GITHUB_PATH"
-}
-
-main "$@"
+winget install --id SWI-Prolog.SWI-Prolog -e --accept-package-agreements --accept-source-agreements
