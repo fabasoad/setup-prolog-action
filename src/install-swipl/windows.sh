491a4850e3242d1747688d6ff4322bc015dcb80d
fabasoad <fabasoad@gmail.com>
2025-10-18 15:47:35 +0900

    fix: opt 4

--- src/install-swipl/windows.sh
+++ src/install-swipl/windows.sh
@@ -2,3 +2,6 @@
 
 #winget install --id SWI-Prolog.SWI-Prolog -e --accept-package-agreements --accept-source-agreements
 choco install swi-prolog
+bin_path="C:\Program Files\swipl\bin"
+ls -la "${bin_path}"
+echo "${bin_path}" >> "$GITHUB_PATH"
